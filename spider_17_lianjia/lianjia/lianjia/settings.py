fad8c650a6ecfaf952de95e417033c1c0f72fa0e
ylnx <799613500@qq.com>
2020-08-10 15:22:14 +0800

    注释掉无用的pipelines

--- spider_17_lianjia/lianjia/lianjia/settings.py
+++ spider_17_lianjia/lianjia/lianjia/settings.py
@@ -65,9 +65,9 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-   'lianjia.pipelines.LianjiaPipeline': 300,
-}
+# ITEM_PIPELINES = {
+#    'lianjia.pipelines.LianjiaPipeline': 300,
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
