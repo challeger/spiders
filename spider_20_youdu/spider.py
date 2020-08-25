4525411c6b9476278f255ff96e9681adbc0ef908
ylnx <799613500@qq.com>
2020-08-25 22:19:52 +0800

    新增 收藏书籍,发送间贴,查看排行榜,搜索接口

--- spider_20_youdu/spider.py
+++ spider_20_youdu/spider.py
@@ -8,6 +8,7 @@
     充值
 """
 import requests
+import threading
 import re
 import base64
 import urllib3
@@ -15,13 +16,15 @@ from bs4 import BeautifulSoup
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
-    'cookie': '登录的cookie',
+    'cookie': 'saveMemberInfo=%7B%22username%22%3A%2218890035791%22%2C%22password%22%3A%22T85568397%22%7D;',
     'origin': 'https://www.youdubook.com',
     'x-requested-with': 'XMLHttpRequest'
 }
 
 
 class Spider_Youdu:
+    _instance_lock = threading.Lock()
+
     def __init__(self):
         super().__init__()
         self.session = requests.session()
@@ -32,12 +35,18 @@ class Spider_Youdu:
         self.my_books = {}
         self.my_wallet = {}
 
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, "_instance"):
+            with cls._instance_lock:
+                if not hasattr(cls, "_instance"):
+                    cls._instance = object.__new__(cls)  
+        return cls._instance
+
     def get_user_center(self):
         # 书架信息
         books_url = 'https://www.youdubook.com/user/favobook'
         # 钱包信息
         money_url = 'https://www.youdubook.com/user/prepaidrecords'
-
         # 获取书架信息
         resp = self.session.get(books_url)
         soup = BeautifulSoup(resp.text, 'lxml')
@@ -47,15 +56,18 @@ class Spider_Youdu:
         resp = self.session.get(money_url)
         soup = BeautifulSoup(resp.text, 'lxml')
         my_wallet = soup.find('div', class_='Top').findAll('li')
+        print(watching_books)
         for book in watching_books:
             book_title = book.find('a')['title']
             new_chapter = book.find('div', class_='updateChapter').find('a')
             self.my_books[book_title] = {
                 '书名': book_title,
-                '书籍链接': book.find('a')['href'],
+                '书籍链接': book.find('a')['href'].split('/')[-1],
+                'book_pic': book.find('img')['data-original'],
                 '最新章节名': new_chapter.get_text().strip(),
                 '最新章节链接': new_chapter['href']
             }
+        print(self.my_books)
         self.my_wallet['推荐票'] = my_wallet[0].find('em').get_text()
         self.my_wallet['月票'] = my_wallet[1].find('em').get_text()
         self.my_wallet['SAN值'] = my_wallet[2].find('em').get_text()
@@ -98,18 +110,30 @@ class Spider_Youdu:
     def get_book_detail(self, book_url):
         resp = self.session.get(book_url)
         soup = BeautifulSoup(resp.text, 'lxml')
-        # 书籍标题+作者
-        book_title = soup.find('div', class_='title')
-        # 书籍简介
-        book_disc = soup.find('div', class_='synopsisCon')
+        title_author = soup.find('div', class_='title')
+        book_data = {
+            'book_title': title_author.find('span').get_text().strip(),
+            'book_author': title_author.find('em').get_text().strip(),
+            'book_disc': soup.find('div', class_='synopsisCon').get_text().strip(),
+            'book_volume_list': []
+        }
         # 书籍卷名
         book_volume_list = soup.findAll('div', class_='volume_name')
-        # 书籍章节列表
         book_chapter_list = soup.findAll('div', class_='chapter_list')
-        print(book_title)
-        print(book_disc)
-        print(book_volume_list)
-        print(book_chapter_list)
+        for volume in book_volume_list:
+            book_data['book_volume_list'].append({
+                'volume_name': volume.get_text().strip(),
+                'chapter_list': []
+            })
+        for volume in book_data['book_volume_list']:
+            for book_chapter in book_chapter_list:
+                chapter_list = book_chapter.findAll('a')
+                for chapter in chapter_list:
+                    volume['chapter_list'].append({
+                        'chapter_title': chapter.get_text().strip(),
+                        'chapter_href': chapter['href']
+                    })
+        print(book_data)
 
     def buy_chapter(self, book_id, chapter_id):
         # 订阅指定章节
@@ -123,7 +147,7 @@ class Spider_Youdu:
             'isAuto': 0
         }
         resp = self.session.post(buy_url, data=data)
-        print(f'章节{chapter_id}|{resp.json()["msg"]}')
+        print(resp.json())
 
     @staticmethod
     def parse_chapter_content(json_data):
@@ -148,12 +172,70 @@ class Spider_Youdu:
             add_time = data['addTime']
             print(f'[{add_time}]{user_name}:{content}')
 
+    def get_rank_html(self, rank_type='Favo', page=1, data_type='Week'):
+        base_url = f'https://www.youdubook.com/ranking/ranklist/tag/{rank_type}/type/{data_type}?page={page}'
+        resp = self.session.get(base_url)
+        soup = BeautifulSoup(resp.text, 'lxml')
+        book_list = soup.find('div', class_='piclist').findAll('li')
+        for book in book_list:
+            # id
+            book_id = book.find('a')['href'].split('/')[-1]
+            # 标题
+            book_title = book.find('a')['title']
+            # 封面
+            book_cover = book.find('img')['data-original']
+            # 作者
+            book_author = book.find('div', class_='nicheng').get_text().strip()
+            # 收藏
+            book_favo = book.find('div', class_='shoucang').get_text().strip()
+            # 人气
+            book_popalrity = book.find('div', class_='renqi').get_text().strip()
+            print('='*30)
+            print(f'{book_id}:{book_title}-{book_author}|{book_cover}|{book_favo}{book_popalrity}')
+
+    def search(self, keyword, page=None):
+        url = f'https://www.youdubook.com/booklibrary/index/str/0_0_0_0_0_0_0_{keyword}?page={page}'
+        resp = self.session.get(url)
+        soup = BeautifulSoup(resp.text, 'lxml')
+        book_list = soup.find('div', class_='BooklibraryList').findAll('li', class_='')[:-1]
+        book_page = soup.find('div', class_='pageInfo').findAll('em')
+        pages = book_page[-3].get_text() if book_page else 0
+        print(pages)
+        for book in book_list:
+            # id
+            book_id = book.find('a')['href'].split('/')[-1]
+            # 标题
+            book_title = book.find('a')['title']
+            # 封面
+            book_cover = book.find('img', class_='img1')['data-original']
+            # 作者
+            book_author = book.find('dd', class_='nickname').get_text().strip()
+            # 收藏
+            book_favo = book.find('dd', class_='favo').get_text().strip()
+            # 人气
+            book_popalrity = book.find('dd', class_='hit').get_text().strip()
+            print('='*30)
+            print(f'{book_id}:{book_title}-{book_author}|{book_cover}|{book_favo}{book_popalrity}')
+
+    def favo_book(self, book_id):
+        url = 'https://www.youdubook.com/booklibrary/actionfavo'
+        data = {
+            'BookID': book_id
+        }
+        resp = self.session.post(url, data)
+        print(resp.json())
+
     def run(self):
+        # self.get_user_center()
         # data = self.get_chapter_json(126427)
+        # print(data)
         # self.parse_chapter_content(data)
         # self.get_book_detail('https://www.youdubook.com/book_detail/1917')
-        data = self.get_line_json(126427, 6, 45)
-        self.parse_line_content(data)
+        # data = self.get_line_json(126427, 6, 45)
+        # self.parse_line_content(data)
+        # self.get_rank_html(rank_type='Hit', data_type='Month', page=2)
+        # self.search('弗雷尔卓德的孤狼')
+        self.favo_book(587)
 
 
 if __name__ == "__main__":
